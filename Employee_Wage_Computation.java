eebd4f195216029d1b5ab5fcfff525589457f52d
Sam7568487077 <118912944+Sam7568487077@users.noreply.github.com>
2022-12-28 10:32:41 +0530

    Delete Employee_Wage_Computation.java

--- Employee_Wage_Computation.java
+++
@@ -1,6 +0,0 @@
-public class Employee_Wage_Computation {
-
-    public static void main(String[] args) {
-        System.out.println(" ====== Welcome to Employee Wage Computation ======");
-    }
-}
